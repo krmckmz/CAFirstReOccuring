b957def34263f31c40ed220cbc052f6f9c5b12d3
krmckmz <keremcokmez@gmail.com>
2022-09-01 23:10:46 +0300

    first commit

---
+++ CAFirstReOccurring/Program.cs
@@ -0,0 +1,21 @@
+﻿
+class Program{
+static void Main(string[] args)
+{
+    var result = GetFirstReOccuredString("ABCA");
+}
+
+public static string GetFirstReOccuredString(string text)
+{
+    Dictionary<char,int> dict = new Dictionary<char,int>();
+
+    for (int i = 0; i < text.Length-1; i++)
+    {
+        if(dict[text[i]] > 1)
+          return text[i].ToString();
+        else
+           dict.Add(text[i],1);
+    }
+    return "null";
+}
+}
